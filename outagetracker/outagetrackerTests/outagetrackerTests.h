900e43a2381100db6e3a0d2b0273a3f66744db3d
halilayyildiz <halil.ayyildiz@gmail.com>
2013-07-21 04:44:43 +0300

    ...

---
+++ outagetracker/outagetrackerTests/outagetrackerTests.h
@@ -0,0 +1,13 @@
+//
+//  outagetrackerTests.h
+//  outagetrackerTests
+//
+//  Created by Halil AYYILDIZ on 7/21/13.
+//  Copyright (c) 2013 Halil AYYILDIZ. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface outagetrackerTests : SenTestCase
+
+@end
---
+++ outagetracker/outagetrackerTests/outagetrackerTests.m
@@ -0,0 +1,32 @@
+//
+//  outagetrackerTests.m
+//  outagetrackerTests
+//
+//  Created by Halil AYYILDIZ on 7/21/13.
+//  Copyright (c) 2013 Halil AYYILDIZ. All rights reserved.
+//
+
+#import "outagetrackerTests.h"
+
+@implementation outagetrackerTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in outagetrackerTests");
+}
+
+@end
