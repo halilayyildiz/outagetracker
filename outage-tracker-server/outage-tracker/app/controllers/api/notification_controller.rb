38259aff26063f0bb57256e293aca9f91e5d239b
halilayyildiz <halil.ayyildiz@gmail.com>
2013-09-04 20:29:06 +0300

    change pem file

--- outage-tracker-server/outage-tracker/app/controllers/api/notification_controller.rb
+++ outage-tracker-server/outage-tracker/app/controllers/api/notification_controller.rb
@@ -5,12 +5,12 @@ class Api::NotificationController < ApplicationController
   def new
 
     APNS.host = 'gateway.sandbox.push.apple.com'
-    APNS.pem  = 'app/assets/apns_dev.pem'
+    APNS.pem = 'app/assets/apns_dev.pem'
     APNS.pass = '1insan'
     device_token = '<ccdf3714 c70b5b88 94725552 1dd23494 e111b4b1 5c094136 479133bb 6e1614aa>'
 
     #APNS.send_notification(device_token, 'Kesinti var yeeenn!')
-    APNS.send_notification(device_token, alert:'Anaammm... Elektrik gitti !!!', badge:1, sound:'default')
+    APNS.send_notification(device_token, alert: 'Anaammm... Elektrik gitti !!!', badge: 1, sound: 'default')
 
   end
 
@@ -19,14 +19,14 @@ class Api::NotificationController < ApplicationController
     user = User.find(params[:id])
 
     APNS.host = 'gateway.push.apple.com'
-    APNS.pem  = 'app/assets/apns_dist.pem'
+    APNS.pem = 'app/assets/outagetracker_apns_production.pem'
     APNS.pass = '1insan'
     device_token = user.push_id
 
     #APNS.send_notification(device_token, 'Kesinti var yeeenn!')
-    APNS.send_notification(device_token, alert:'Anaammm... Elektrik gitti !!!', badge:1, sound:'default')
+    APNS.send_notification(device_token, alert: 'Anaammm... Elektrik gitti !!!', badge: 1, sound: 'default')
 
-    render json:user
+    render json: user
   end
 
   def message2
@@ -34,14 +34,14 @@ class Api::NotificationController < ApplicationController
     user = User.find(params[:id])
 
     APNS.host = 'gateway.sandbox.push.apple.com'
-    APNS.pem  = 'app/assets/apns_dev.pem'
+    APNS.pem = 'app/assets/outagetracker_apns_development.pem'
     APNS.pass = '1insan'
     device_token = user.push_id
 
     #APNS.send_notification(device_token, 'Kesinti var yeeenn!')
-    APNS.send_notification(device_token, alert:'Anaammm... Elektrik gitti !!!', badge:1, sound:'default')
+    APNS.send_notification(device_token, alert: 'Anaammm... Elektrik gitti !!!', badge: 1, sound: 'default')
 
-    render json:user
+    render json: user
   end
 
 end
